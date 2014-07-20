bef933b571a60efc9f7c3fb436a2b94d795cea9e
zengohm <zengohm@gmail.com>
2014-07-20 18:49:53 +0800

    Change title and version

--- dist/jquery.include.js
+++ dist/jquery.include.js
@@ -1,46 +1,51 @@
-/*! Include - v0.1.0 - 2014-07-20
+/*! jQuery Include - v0.1.1 - 2014-07-20
 * https://github.com/zengohm/jquery-include
-* Copyright (c) 2014 zengohm; Licensed MIT */
-(function($) {
-  var makePathName = function(base,path){
-      base = arguments[0]?arguments[0]:location.pathname;
-      path = arguments[1]?arguments[1]:'';
-      if(path[0]==='/' || path.match(/^\w+\:\/\//)){
-          return path;
-      }else {
-          return base.substr(0, base.lastIndexOf('/')) + '/' + path;
-      }
-  };
-  // Collection method.
-  $.fn.include = function() {
-    return this.each(function() {
-      // Do something awesome to each selected element.
-      $(this).find('[jq-include]').not('[jq-include-ignore]').each(function(){
-        var obj = $(this);
-        var parentFrom = obj.parents('[jq-include]:first').attr('jq-include-from');
-        if(!parentFrom){
-            parentFrom = makePathName();
+* Copyright (c) 2014 Zeng Ohm; Licensed MIT */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    var makePathName = function (base, path) {
+        base = arguments[0] ? arguments[0] : location.pathname;
+        path = arguments[1] ? arguments[1] : '';
+        if (path[0] === '/' || path.match(/^\w+\:\/\//)) {
+            return path;
+        } else {
+            return base.substr(0, base.lastIndexOf('/')) + '/' + path;
         }
-        var url = makePathName(parentFrom,$(this).attr('jq-include'));
-        obj.attr('jq-include-from',makePathName(url)).attr('jq-include-ignore',true);
+    };
 
-        $(this).html($.ajax({'async':false,'method':'get','url':url}).responseText);
-        $(this).include();
-      });
-    });
-  };
+    $.fn.include = function () {
+        return this.each(function () {
+            $(this).find('[jq-include]').not('[jq-include-ignore]').each(function () {
+                var obj = $(this);
+                var parentFrom = obj.parents('[jq-include]:first').attr('jq-include-from');
+                if (!parentFrom) {
+                    parentFrom = makePathName();
+                }
+                var url = makePathName(parentFrom, $(this).attr('jq-include'));
+                obj.attr('jq-include-from', makePathName(url)).attr('jq-include-ignore', true);
 
-  // Static method.
-  $.include = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.include.options, options);
-    // Return something awesome.
-    return $('body').include();
-  };
+                $(this).html($.ajax({'async': false, 'method': 'get', 'url': url}).responseText);
+                $(this).include();
+            });
+        });
+    };
 
-  // Static method default options.
-  $.include.options = {
-  };
+    $.include = function (options) {
+        options = $.extend({}, $.include.options, options);
+        return $('body').include();
+    };
 
+    $.include.options = {
+    };
 
-}(jQuery));
+}));
